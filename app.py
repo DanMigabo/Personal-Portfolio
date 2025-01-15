9bfd846cb60c2d51e0ba703de22527f51c0b2953
Dan Migabo <migabo.dan@gmail.com>
2025-01-15 14:19:50 +0200

    Sync local repository with GitHub

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, render_template
 
 # Initialize Flask app
@@ -14,21 +13,4 @@ def about():
 # Run the app
 if __name__ == "__main__":
     # Ensure the app runs with debug mode enabled (useful for development)
-=======
-from flask import Flask, render_template
-
-# Initialize Flask app
-app = Flask(__name__)
-
-# Endpoints
-@app.route('/')
-def home():
-    return render_template('index.html')
-@app.route('/about')
-def about():
-    return render_template('about.html')
-# Run the app
-if __name__ == "__main__":
-    # Ensure the app runs with debug mode enabled (useful for development)
->>>>>>> 3aff6891c6e2c564eb232a3e2e8060646fdb9ec1
     app.run(debug=True)
